d7a7b4faffb9b8d12bdf0b48ffc67c4bb8813c89
Dario Gosmar <119061629+d4darius@users.noreply.github.com>
2025-05-06 16:03:09 +0200

    loss debug script

--- lib/loss.py
+++ lib/loss.py
@@ -54,6 +54,10 @@ def loss_calculation(pred_r, pred_t, pred_c, target, model_points, idx, points, 
     how_max, which_max = torch.max(pred_c, 1)
     dis = dis.view(bs, num_p)
 
+    print("which_max:", which_max)
+    print("ori_t shape:", ori_t.shape)
+    print("points shape:", points.shape)
+
     t = ori_t[which_max[0]] + points[which_max[0]]
     points = points.view(1, bs * num_p, 3)
 
