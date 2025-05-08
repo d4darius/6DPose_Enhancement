56d37f613d48376cde1e671c3d476904eb18cad8
Dario Gosmar <119061629+d4darius@users.noreply.github.com>
2025-05-08 22:55:05 +0200

    Refactor loss calculation to improve target selection and added GPU friendly simmetry handling

--- lib/loss.py
+++ lib/loss.py
@@ -1,16 +1,8 @@
 from torch.nn.modules.loss import _Loss
-from torch.autograd import Variable
 import torch
-import time
-import numpy as np
-import torch.nn as nn
-import random
-import torch.backends.cudnn as cudnn
-from lib.knn_utils import KNearestNeighbor
 
 
 def loss_calculation(pred_r, pred_t, pred_c, target, model_points, idx, points, w, refine, num_point_mesh, sym_list):
-    knn = KNearestNeighbor(1)
     bs, num_p, _ = pred_c.size()
 
     pred_r = pred_r / (torch.norm(pred_r, dim=2).view(bs, num_p, 1))
@@ -39,25 +31,27 @@ def loss_calculation(pred_r, pred_t, pred_c, target, model_points, idx, points, 
 
     if not refine:
         if idx[0].item() in sym_list:
-            target = target[0].transpose(1, 0).contiguous().view(3, -1)
-            pred = pred.permute(2, 0, 1).contiguous().view(3, -1)
-            inds = knn(target.unsqueeze(0), pred.unsqueeze(0))
-            target = torch.index_select(target, 1, inds.view(-1) - 1)
-            target = target.view(3, bs * num_p, num_point_mesh).permute(1, 2, 0).contiguous()
-            pred = pred.view(3, bs * num_p, num_point_mesh).permute(1, 2, 0).contiguous()
+            pred_exp = pred.unsqueeze(2)
+            target_exp = target.unsqueeze(1)
 
+            dist_matrix = torch.norm(pred_exp - target_exp, dim=3)
+            nearest_indices = dist_matrix.argmin(dim=2)
+            batch_indices = torch.arange(pred.size(0), device=pred.device).unsqueeze(1).repeat(1, pred.size(1))
+
+            target = target[batch_indices, nearest_indices]
+            
     dis = torch.mean(torch.norm((pred - target), dim=2), dim=1)
     loss = torch.mean((dis * pred_c - w * torch.log(pred_c)), dim=0)
-    
 
     pred_c = pred_c.view(bs, num_p)
-    how_max, which_max = torch.max(pred_c, 1)
+    which_max = pred_c.argmax(dim=1)
+    which_max = which_max.clamp(0, points.size(0) - 1)
     dis = dis.view(bs, num_p)
 
-    t = ori_t[which_max[0]] + points[which_max[0]]
+    t = (ori_t + points)[which_max][0]
     points = points.view(1, bs * num_p, 3)
 
-    ori_base = ori_base[which_max[0]].view(1, 3, 3).contiguous()
+    ori_base = ori_base[which_max].view(1, 3, 3).contiguous()
     ori_t = t.repeat(bs * num_p, 1).contiguous().view(1, bs * num_p, 3)
     new_points = torch.bmm((points - ori_t), ori_base).contiguous()
 
@@ -66,8 +60,7 @@ def loss_calculation(pred_r, pred_t, pred_c, target, model_points, idx, points, 
     new_target = torch.bmm((new_target - ori_t), ori_base).contiguous()
 
     # print('------------> ', dis[0][which_max[0]].item(), pred_c[0][which_max[0]].item(), idx[0].item())
-    del knn
-    return loss, dis[0][which_max[0]], new_points.detach(), new_target.detach()
+    return loss, dis[0][which_max], new_points.detach(), new_target.detach()
 
 
 class Loss(_Loss):
